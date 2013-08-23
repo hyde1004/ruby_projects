e4bfbd8e5665c232270c50c48495e44c62f14b04
hyde1004 <hyde1004@gmail.com>
2013-08-23 19:04:19 +0900

    improve

--- scan_info/scan_info.rb
+++ scan_info/scan_info.rb
@@ -7,29 +7,46 @@
 
 raw_file = File.open("d:\\log\\channel_info.txt")
 
+count = 0
 raw_file.each_line do |line|
+	if (count%3 == 0)
+		count = 0
+	end
+
+	service = Hash.new
+
 	if (line =~ /^[0-9][0-9][0-9][0-9]/)
-		ch_order = puts line.match('^[0-9][0-9][0-9][0-9]')
-		puts ch_order
+		ch_order = line.match('^[0-9][0-9][0-9][0-9]')
+		#puts ch_order
+		service[:order] = ch_order.to_s
 	end
 
 	if (line =~ /v->..../)
 		v_pid = line.match('v->....')
-		puts v_pid
+		#puts v_pid
+		service[:v_pid] = v_pid.to_s
 	end
 
 	if (line =~ /a->..../)
 		a_pid = line.match('a->....')
-		puts a_pid
+		#puts a_pid
+		service[:a_pid] = a_pid.to_s
 	end
 
 	if (line =~ /pcr->..../)
 		pcr_pid = line.match('pcr->....')
-		puts pcr_pid
+		#puts pcr_pid
+		service[:pcr_pid] = pcr_pid.to_s
 	end
 
 	if (line =~ /ServiceName\s:\s/)
 		service_name = line.match('ServiceName\s:\s.*')
-		puts service_name
+		#puts service_name
+		service[:service_name] = service_name.to_s
+	end
+
+	count = count + 1
+	if (count == 3)
+		puts service
 	end
 end
